9a1920768345fb88e1c13e8db6b8320ae00bc94a
Ramon Lee <ramonqlee>
2012-07-29 06:39:55 +0800

    init release

---
+++ AmzPhoto/AmzPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AmzPhoto
+//
+//  Created by Lee Ramon on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
